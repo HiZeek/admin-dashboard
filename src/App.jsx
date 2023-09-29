6b05ef68750bfa7bafbfe3397fb6fe2d9ce3a1c9
Isaac Idibia <idibiaisaac3@gmail.com>
2023-09-29 15:57:34 +0100

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import { Route, Routes } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import SidebarApp from "./scenes/global/Sidebar";
+// import Team from "./scenes/global/team";
+// import Invoices from "./scenes/global/invoices";
+// import Contacts from "./scenes/global/contacts";
+// import Bar from "./scenes/global/bar";
+// import Form from "./scenes/global/form";
+// import Line from "./scenes/global/line";
+// import Pie from "./scenes/global/pie";
+// import FAQ from "./scenes/global/faq";
+// import Geography from "./scenes/global/geography";
+// import Calendar from "./scenes/global/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
-  return <div className="app"></div>;
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <SidebarApp />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              {/* <Route exact path="/team" element={<Team />} /> */}
+              {/* <Route exact path="/contacts" element={<Contacts />} /> */}
+              {/* <Route exact path="/invoices" element={<Invoices />} /> */}
+              {/* <Route exact path="/form" element={<Form />} /> */}
+              {/* <Route exact path="/bar" element={<Bar />} /> */}
+              {/* <Route exact path="/pie" element={<Pie />} /> */}
+              {/* <Route exact path="/line" element={<Line />} /> */}
+              {/* <Route exact path="/faq" element={<FAQ />} /> */}
+              {/* <Route exact path="/geography" element={<Geography />} /> */}
+              {/* <Route exact path="/calendar" element={<Calendar />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
